58d3f1e0e62a6da18ca83dd4cf5cf340924bc56e
A.H.Yousefi <AH.Yousefi@espandarco.com>
2024-07-31 10:32:55 +0330

    add header

--- BookStoreClean2/bookstore-frontend/src/User/LoginUser.js
+++ BookStoreClean2/bookstore-frontend/src/User/LoginUser.js
@@ -2,7 +2,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import {useNavigate} from "react-router-dom";
-import Header from "../../Component/Header";
+import Header from "../Component/Header";
 
 const LoginUser = () => {
     const [username, setUsername] = useState('');
